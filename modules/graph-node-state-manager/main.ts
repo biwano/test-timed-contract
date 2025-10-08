c4b7b020ecc3d1043543ea1b38a5d0a8d445b029
biwano <biwano@gmail.com>
2025-10-08 16:14:58 +0200

    renamed

--- modules/graph-node-state-manager/main.ts
+++ modules/graph-node-state-manager/main.ts
@@ -7,8 +7,8 @@ import { removeCommand } from "./commands/remove.ts";
 import { deployCommand } from "./commands/deploy.ts";
 
 const main = new Command()
-  .name("anvil-event-faker")
-  .description("A tool to create fake contracts that only emit events based on subgraph definitions")
+  .name("graph-node-state-manager")
+  .description("Manage fake contracts and deploy them to match a subgraph's state")
   .version("1.0.0")
   .command("init", initCommand)
   .command("generate", generateCommand)
